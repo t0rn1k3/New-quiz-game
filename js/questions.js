2a86374c1a94a96a407075f0e09ccd24c4dcc5ef
t0rn1k3 <tornikeburjanadze2@gmail.com>
2024-05-16 20:30:58 +0400

    add questions with options and answers in questions array

---
+++ js/questions.js
@@ -0,0 +1,57 @@
+const questions = [
+    {
+        number: 1,
+        question : "What was Charlie Parker's(Jazz musician) nickname?",
+        answer: 'Bird',
+        options: [
+            "Magician",
+            "Bird",
+            "Solist",
+            "God",
+        ]
+    },
+    {
+        number: 2,
+        question : 'Who is Dali?',
+        answer: 'Goddes of hunt',
+        options: [
+            "Goddes of rain",
+            "Goddes of hunt",
+            "Goddes of war",
+            "My neighbour",
+        ]
+    },
+    {
+        number: 3,
+        question : '"When the child was a child, it did not know, that it was a child..." Which movie starts with this quote?',
+        answer: 'Wings of desire',
+        options: [
+            "Wings of desire",
+            "Children of Men",
+            "K-pax",
+            "V for Vendetta",
+        ]
+    },
+    {
+        number: 4,
+        question : '"All we are is dust in the wind" which rock band sang this lyrics?',
+        answer: 'Kansas',
+        options: [
+            "Pink Floyd",
+            "Metallica",
+            "Scorpion",
+            "Kansas"
+        ]
+    },
+    {
+        number: 5,
+        question : 'Which creature has three heads?',
+        answer: 'Cerber',
+        options: [
+            "Cerber",
+            "Basilisk",
+            "Centaur",
+            "Griffin"
+        ]
+    },
+]
