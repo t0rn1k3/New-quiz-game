a1925a1a944a1e6c2eb3d73b35afc0a57e389375
t0rn1k3 <tornikeburjanadze2@gmail.com>
2024-05-16 20:04:24 +0400

    build quiestins box

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,9 @@
 const startQuiz = document.querySelector('#startButton');
 const infoContainer = document.querySelector('.info-container');
 const exitButton = document.querySelector('#exitButton');
+const btnContinue = document.querySelector('#continue')
 
+const quizSection = document.querySelector('.quiz-section')
 const main = document.querySelector('.main');
 
 //open quiz guide popup
@@ -13,4 +15,9 @@ startQuiz.addEventListener('click', ()=> {
 exitButton.addEventListener('click' ,()=> {
     infoContainer.classList.remove('active');
     main.classList.remove('active');
+})
+//show up quiz section
+btnContinue.addEventListener('click', ()=> {
+    quizSection.classList.add('active');
+    infoContainer.classList.remove('active');
 })
